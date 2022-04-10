69bbdb1fc444b17e90394ce580bb415b0add4e29
Ryan Caulfield <Ryancaulfield3103@gmail.com>
2022-04-10 17:22:25 +0100

    added more fyne styling to the project

--- main.go
+++ main.go
@@ -1,14 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+	"image/color"
+	"net/http"
+	"time"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+var client *http.Client
 
-	w.SetContent(widget.NewLabel("Hello World"))
-	w.ShowAndRun()
+type randomFact struct {
+	Text string `json:"test"`
+}
+
+func getRandomFacts() (randomFact, error) {
+	var fact randomFact
+	resp, err := client.Get("https://uselessfacts.jsph.pl/random.json?language=en")
+	if err != nil {
+		return randomFact{}, err
+	}
+
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&fact)
+	if err != nil {
+		return randomFact{}, err
+	}
+
+	return fact, nil
+}
+
+func main() {
+	client = &http.Client{Timeout: 10 * time.Second}
+
+	a := app.New()
+	win := a.NewWindow("Get Useless Fact")
+	win.Resize(fyne.NewSize(800, 300))
+
+	title := canvas.NewText("Get Useless Fact", color.White)
+	title.TextStyle = fyne.TextStyle{Bold: true}
+
+	factText := widget.NewLabel("")
+	factText.Wrapping = fyne.TextWrapWord
+
+	win.SetContent(widget.NewLabel("Hello World"))
+	win.ShowAndRun()
 }
